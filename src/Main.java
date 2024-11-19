d1eed8415aa2788fbb20e78b4aabcb5efa579647
Yves Andres <andreselianordam@mail.com>
2024-11-19 19:47:31 +0100

    this is the time of the year

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import com.google.gson.Gson;
+
+import java.io.Serializable;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello 1st commit!");
+
+        String json = "{\"name\": \"davor suker\"}";
+        Gson firstPlayer = new Gson();
+
+        Student s = new Student();
+        s = firstPlayer.fromJson(json, Student.class);
+        System.out.println(s.name);
     }
+}
+
+class Student implements Serializable {
+    public String name;
 }
